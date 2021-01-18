3f9f7cff0fa75b68f73472cab60738ddb964e903
Banee Ishaque K <baneeishaque@gmail.com>
2021-01-19 04:18:26 +0530

    In

---
+++ app/src/main/java/stack/overflow/EmptyActivity.java
@@ -0,0 +1,13 @@
+package stack.overflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
